84149403f96e1aa07ed9583187d35374051ba490
fadosu <fadosu@ttu.ee>
2020-11-07 13:41:58 +0200

    added Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import MainPage from "./components/MainPage/MainPage";
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Profile from './components/UserProfile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage/>
-    </div>
+    <Router>
+      <div className="App">
+        <switch>
+          <Route path="/" component={MainPage} />
+          <Route path="/Profile" component={Profile} />
+        </switch>
+      </div>
+    </Router>
   );
 }
 
