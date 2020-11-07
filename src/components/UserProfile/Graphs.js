3fc03df1654cf0c5b0403014dff82185d897ec35
fadosu <fadosu@ttu.ee>
2020-11-07 18:21:18 +0200

    Graphs

---
+++ src/components/UserProfile/Graphs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Profile.css';
+
+const Graphs = () => {
+  return (
+    <div className="graph">
+      Graph
+    </div>
+  );
+};
+
+export default Graphs;
--- src/components/UserProfile/Profile.js
+++ src/components/UserProfile/Profile.js
@@ -1,18 +1,24 @@
 import React from "react";
-import ProfileHeader from "./ProfileHeader";
+import ProfileContent from "./ProfileContent";
+import Graphs from "./Graphs"
 import HeaderProfilePage from "./HeaderProfilePage"
 import Footer from "../Footer/Footer";
+import './Profile.css';
 
 
-export default function Profile ({productList, setProductList}) {
-  return(
-      <div>
-        <HeaderProfilePage
-            productList={productList}
-            setProductList={setProductList}
-        />
-        <ProfileHeader/>
-        <Footer/>
+export default function Profile({ productList, setProductList }) {
+  return (
+    <div>
+      <HeaderProfilePage
+        productList={productList}
+        setProductList={setProductList}
+      />
+
+      <div className="profilebody">
+        <ProfileContent />
+        <Graphs />
       </div>
+      <Footer />
+    </div>
   )
 }
