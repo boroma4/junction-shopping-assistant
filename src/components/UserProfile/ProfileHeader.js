1081d4c3f51849457d85fb03aeab03bf0019b3f0
fadosu <fadosu@ttu.ee>
2020-11-07 13:41:26 +0200

    added userProfile

---
+++ src/components/UserProfile/ProfileHeader.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Segment,
+  Item,
+  Header,
+  Button,
+  Grid,
+  Statistic,
+  Divider,
+  Reveal,
+} from "semantic-ui-react";
+
+const ProfileHeader = () => {
+  return (
+    <Segment>
+      <Grid>
+        <Grid.Column width={12}>
+          <Item.Group>
+            <Item>
+              <Item.Image
+                avatar
+                size="small"
+                src={profile.image || "/assets/user.png"}
+              />
+              <Item.Content verticalAlign="middle">
+                <Header as="h1">{profile.displayName}</Header>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <Statistic.Group widths={2}>
+            <Statistic label="Followers" value={profile.followersCount} />
+            <Statistic label="Following" value={profile.followingCount} />
+          </Statistic.Group>
+          <Divider />
+        </Grid.Column>
+      </Grid>
+    </Segment>
+  );
+};
+
+export default observer(ProfileHeader);
