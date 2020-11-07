5473535aa6df14bb2c0a3aa667c593f3a8b60f1c
fadosu <fadosu@ttu.ee>
2020-11-07 21:56:17 +0200

    very shitty Profile

--- src/components/UserProfile/Graphs.js
+++ src/components/UserProfile/Graphs.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import './Profile.css';
 import { VictoryPie } from "victory-pie";
-
 import { useHistory } from "react-router-dom";
+import HistogramGraph from "./HistogramGraph"
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
 const myData = [
-    { x: "Body", y: 900 },
-    { x: "Toiletries", y: 400 },
-    { x: "Kitchen", y: 300 },
+    
+    { x: "$100 Toilet", y: 400 },
+    { x: "$300 Bathroom", y: 900 },
+    { x: "$150 Miscellaneous", y: 300 },
 ];
 
 
@@ -15,29 +18,32 @@ const Graphs = () => {
 
     let history = useHistory();
 
+    const [isHistogram, setIsHistogram] = useState(false);
+
     return (
-        <div className="graph">
-            <VictoryPie
-                data={myData}
-                colorScale={["blue", "yellow", "red"]}
-                radius={100}
-                events={[{
-                    target: "data",
-                    eventHandlers: {
-                      onClick: () => {
-                          return [
-                              {
-                                target: "data",
-                                mutation: ({ Link }) => {
-                                    return  history.push("/Histogram");
-                                  }
-                              }
-                          ]
-                      }
+        <Container >
+            <Grid className="graph" container spacing={2} alignContent={"center"}>
+                <Grid item xs={6}>
+                    <VictoryPie
+                        data={myData}
+                        colorScale={["blue", "yellow", "red"]}
+                        radius={100}
+                        events={[{
+                            target: "data",
+                            eventHandlers: {
+                                onClick: () => { setIsHistogram(true) }
+                            }
+                        }]}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    {isHistogram ?
+                        (<HistogramGraph />) :
+                        (<></>)
                     }
-                  }]}
-            />
-        </div>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
